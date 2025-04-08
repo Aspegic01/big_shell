4522d8f9eddf4886f59cab0010d5c2d596191c31
Mouad Labrirhil <mlabrirh@e2r6p10.1337.ma>
2025-04-08 11:04:47 +0100

    utils token

---
+++ includes/minishell.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlabrirh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/06 13:49:55 by mlabrirh          #+#    #+#             */
+/*   Updated: 2025/04/06 13:51:08 by mlabrirh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	MINISHELL_H
+#define MINISHELL_H
+
+# include <unistd.h>
+# include <errno.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <limits.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include "../libft/libft.h"
+
+typedef enum
+{
+	TOKEN_WORD,
+	TOKEN_REDIR_OUT,
+	TOKEN_REDIR_IN,
+	TOKEN_PIPE,
+	TOKEN_REDIR_APPEND,
+	TOKEN_HEREDOC
+} token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	token_type		type;
+	struct s_token	*next;
+} t_token;
+
+typedef struct s_args
+{
+	char	**args;
+	token_type	type;
+	int			arg_size;
+	int			heredoc;
+	struct	s_args	*next;
+} t_args;
+
+t_token	*add_token(t_token **token_list, char *value, token_type type);
+
+#endif
---
+++ sources/utils_token.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_token.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlabrirh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 19:09:00 by mlabrirh          #+#    #+#             */
+/*   Updated: 2025/04/07 19:17:01 by mlabrirh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+t_token	*add_token(t_token **token_list, char *value, token_type type)
+{
+	t_token	*new_token;
+	t_token	*current;
+
+	new_token = malloc(sizeof(t_token));
+	if (!new_token)
+		return (NULL);
+	new_token->value = ft_strdup(value);
+	new_token->type = type;
+	new_token->next = NULL;
+
+	if (*token_list == NULL)
+	{
+		*token_list = new_token;
+		return (new_token);
+	}
+	current = *token_list;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new_token;
+	return (new_token);
+}
