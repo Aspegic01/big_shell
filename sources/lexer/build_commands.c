98333397255b78f56e322ea229cb027d7cd0f335
Mouad Labrirhil <mlabrirh@c4r10p9.1337.ma>
2025-04-29 21:24:06 +0100

    handle assignment

--- sources/lexer/build_commands.c
+++ sources/lexer/build_commands.c
@@ -14,11 +14,13 @@
 
 void check_and_set_assignment(t_token *token)
 {
+	char	*equal_sign;
+
     while (token)
     {
         if (token->type == TOKEN_WORD)
         {
-            char *equal_sign = strchr(token->value, '=');
+            equal_sign = strchr(token->value, '=');
             if (equal_sign && equal_sign != token->value)
             {
                 token->type = TOKEN_ASSIGNMENT;
