afebbd55fc4616ffcc073e4319ebdae70a37e254
Narancia34 <tahagamraoui@gmail.com>
2025-04-29 14:27:21 +0000

    handled append redirection + organised some files

--- exec/check_input.c
+++ exec/check_input.c
@@ -6,7 +6,7 @@
 /*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 14:11:27 by mgamraou          #+#    #+#             */
-/*   Updated: 2025/04/27 15:52:23 by mgamraou         ###   ########.fr       */
+/*   Updated: 2025/04/29 14:16:00 by mgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,52 +22,6 @@ int	has_pipe(t_command *input)
 		res++;
 	}
 	return (res);
-}
-
-int	is_builtin(char *arg)
-{
-	int		i;
-	char *builtins[] = {"echo", "cd", "pwd", "export", "unset", "env", "exit", NULL};
-
-	if (!arg)
-		return 0;
-	i = 0;
-	while (builtins[i])
-	{
-		if (strcmp(arg, builtins[i]) == 0)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-void	exec_builtin(char **arg, t_env *env_list, char **o_args)
-{
-	if (!arg || !arg[0])
-		return ;
-	int saved_stdout = dup(STDOUT_FILENO);
-	int saved_stdin = dup(STDIN_FILENO);
-	if (redirect_in(o_args) != 0) {
-		ft_putstr_fd("minishell: redirection error\n", 2);
-		dup2(saved_stdout, STDOUT_FILENO);  // Restore stdout
-		close(saved_stdout);
-		close(saved_stdin);
-		return;
-	}
-	if (ft_strcmp(arg[0], "echo") == 0)
-		ft_echo(arg);
-	if (ft_strcmp(arg[0], "cd") == 0)
-		ft_cd(arg);
-	if (ft_strcmp(arg[0], "pwd") == 0)
-		ft_pwd();
-	if (ft_strcmp("env", arg[0]) == 0)
-		ft_env(env_list);
-	if (ft_strcmp("exit", arg[0]) == 0)
-		exit(0);
-	dup2(saved_stdout, STDOUT_FILENO);
-	dup2(saved_stdin, STDIN_FILENO);
-	close(saved_stdout);
-	close(saved_stdin);
 }
 
 char	**get_cmd(char **o_args)
@@ -94,7 +48,7 @@ char	**get_cmd(char **o_args)
 	j = 0;
 	while (o_args[i])
 	{
-		if ((ft_strcmp(o_args[i], ">") == 0 || ft_strcmp(o_args[i], "<") == 0) && o_args[i+1])
+		if ((ft_strcmp(o_args[i], ">") == 0 || ft_strcmp(o_args[i], "<") == 0 || ft_strcmp(o_args[i], ">>") == 0) && o_args[i+1])
 			i++;
 		else
 		{
@@ -117,7 +71,6 @@ void	check_input(t_command *input, t_env *env_list, char **envp)
 	else
 	{
 		tmp = input;
-		printf("\nno pipe\n");
 		while (tmp)
 		{
 			args = get_cmd(tmp->args);
--- exec/execute_cmd.c
+++ exec/execute_cmd.c
@@ -6,11 +6,12 @@
 /*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 10:17:15 by mgamraou          #+#    #+#             */
-/*   Updated: 2025/04/27 15:51:49 by mgamraou         ###   ########.fr       */
+/*   Updated: 2025/04/29 14:03:52 by mgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include <fcntl.h>
 
 void	handle_exec(char *path, char **args, char **envp)
 {
@@ -26,32 +27,6 @@ void	handle_exec(char *path, char **args, char **envp)
 		clean_up(path, args);
 		exit(126);
 	}
-}
-
-int	redirect_in(char **args)
-{
-	int	fd_in;
-	int	fd_out;
-	int	i;
-
-	i = 0;
-	while (args[i])
-	{
-		if (ft_strcmp(args[i], ">") == 0)
-		{
-			fd_in = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			dup2(fd_in, STDOUT_FILENO);
-			close(fd_in);
-		}
-		else if (ft_strcmp(args[i], "<") == 0)
-		{
-			fd_out = open(args[i + 1], O_RDONLY);
-			dup2(fd_out, STDIN_FILENO);
-			close(fd_out);
-		}
-		i++;
-	}
-	return (0);
 }
 
 void	exec_cmd(char **args, char **envp, char **o_args, int has_pipe)
