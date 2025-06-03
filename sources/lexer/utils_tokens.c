fbc71c90390bf6ad6d27600590e53fcace47cc22
Aspegic <mlabrirh@student.1337.ma>
2025-06-03 03:41:35 -0700

    Update utils_tokens.c
    handle quote

--- sources/lexer/utils_tokens.c
+++ sources/lexer/utils_tokens.c
@@ -12,67 +12,66 @@
 
 #include "../../includes/minishell.h"
 
-t_token	*ft_add_token(t_token **token_list, char *value, token_type type)
+t_token *ft_add_token(t_token **token_list, char *value, token_type type)
 {
-	t_token	*new_token;
-	t_token	*current;
+        t_token *new_token;
+        t_token *current;
 
-	new_token = malloc(sizeof(t_token));
-	if (!new_token)
-		return (NULL);
-	new_token->value = ft_strdup(value);
-	if (!new_token->value) {
-		free(new_token);
-		return (NULL);
-	}
-	new_token->type = type;
-	new_token->next = NULL;
-	if (*token_list == NULL)
-	{
-		*token_list = new_token;
-		return (new_token);
-	}
-	current = *token_list;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new_token;
-	return (new_token);
+        new_token = malloc(sizeof(t_token));
+        if (!new_token)
+                return (NULL);
+        new_token->value = ft_strdup(value);
+        if (!new_token->value) {
+                free(new_token);
+                return (NULL);
+        }
+        new_token->type = type;
+        new_token->next = NULL;
+        if (*token_list == NULL)
+        {
+                *token_list = new_token;
+                return (new_token);
+        }
+        current = *token_list;
+        while (current->next != NULL)
+                current = current->next;
+        current->next = new_token;
+        return (new_token);
 }
 
-char	*read_operator(const char *str, int *i)
+char    *read_operator(const char *str, int *i)
 {
-	int	start;
+        int     start;
 
-	start = *i;
-	if ((str[*i] == '>' || str[*i] == '<') && str[*i] == str[*i + 1])
-		*i += 2;
-	else
-		(*i)++;
-	return (ft_strndup(&str[start], *i - start));
+        start = *i;
+        if ((str[*i] == '>' || str[*i] == '<') && str[*i] == str[*i + 1])
+                *i += 2;
+        else
+                (*i)++;
+        return (ft_strndup(&str[start], *i - start));
 }
-
 static char *append_content(char *existing, const char *new_content, int len)
 {
     char *tmp;
     char *result;
 
-    if (!new_content || len <= 0)
+    if (!new_content || len <= 0) {
         return existing;
+    }
+
     tmp = ft_strndup(new_content, len);
     if (!tmp) {
-        free(existing);
-        return NULL;
+        return existing;
     }
-    if (existing)
-	{
+
+    if (existing) {
         result = ft_strjoin(existing, tmp);
         free(existing);
         free(tmp);
-        if (!result) return NULL;
         return result;
-	}
-    else
+    } else {
         return tmp;
+    }
 }
 
 static char *handle_quoted_section(const char *str, int *i, char *current_content)
@@ -83,32 +82,33 @@ static char *handle_quoted_section(const char *str, int *i, char *current_conten
     quoted = read_quoted((char *)str, i);
     if (!quoted) {
         free(current_content);
-		return NULL;
+        return NULL;
     }
-	if (current_content)
-	{
+
+    if (current_content) {
         new_content = ft_strjoin(current_content, quoted);
         free(current_content);
         free(quoted);
-        if (!new_content) return NULL;
         return new_content;
-	}
-	else
+    } else {
         return quoted;
+    }
 }
 
 static char *handle_unquoted_section(const char *str, int *i, int *start, char *current_content)
 {
-    char *result = current_content;
-    while (str[*i] && !ft_whitespace(str[*i]) && 
-           !ft_is_operator(str[*i]) && 
+    while (str[*i] && !ft_whitespace(str[*i]) &&
+           !ft_is_operator(str[*i]) &&
            str[*i] != '\'' && str[*i] != '"')
+    {
         (*i)++;
-    if (*i > *start)
-	{
-        result = append_content(current_content, &str[*start], *i - *start);
     }
-    return result;
+
+    if (*i > *start) {
+        return append_content(current_content, &str[*start], *i - *start);
+    } else {
+        return current_content;
+    }
 }
 
 char *read_word(const char *str, int *i)
@@ -121,31 +121,33 @@ char *read_word(const char *str, int *i)
         if (str[*i] == '\'' || str[*i] == '"')
         {
             content = handle_unquoted_section(str, i, &start, content);
-            if (!content) return NULL;
             content = handle_quoted_section(str, i, content);
-            if (!content) return NULL;
             start = *i;
         }
         else
+        {
             (*i)++;
+        }
     }
     content = handle_unquoted_section(str, i, &start, content);
-    if (!content)
-        return NULL;
-    return content;
+    if (content) {
+        return content;
+    } else {
+        return ft_strdup("");
+    }
 }
 
-token_type	get_operation_type(const char *op)
+token_type      get_operation_type(const char *op)
 {
-	if (ft_strcmp(op, ">") == 0)
-		return (TOKEN_REDIR_OUT);
-	if (ft_strcmp(op, ">>") == 0)
-		return (TOKEN_REDIR_APPEND);
-	if (ft_strcmp(op, "<") == 0)
-		return (TOKEN_REDIR_IN);
-	if (ft_strcmp(op, "<<") == 0)
-		return (TOKEN_HEREDOC);
-	if (ft_strcmp(op, "|") == 0)
-		return (TOKEN_PIPE);
-	return (TOKEN_WORD);
+        if (ft_strcmp(op, ">") == 0)
+                return (TOKEN_REDIR_OUT);
+        if (ft_strcmp(op, ">>") == 0)
+                return (TOKEN_REDIR_APPEND);
+        if (ft_strcmp(op, "<") == 0)
+                return (TOKEN_REDIR_IN);
+        if (ft_strcmp(op, "<<") == 0)
+                return (TOKEN_HEREDOC);
+        if (ft_strcmp(op, "|") == 0)
+                return (TOKEN_PIPE);
+        return (TOKEN_WORD);
 }
